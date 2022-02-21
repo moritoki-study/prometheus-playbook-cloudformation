48bb3076feba98c6107d7b6640ec99cd594ddaad
moritoki-study <moritot@cocoto.co.jp>
2022-02-22 00:14:26 +0900

    test

--- roles/test_httpserver/files/goenv.sh
+++ roles/test_httpserver/files/goenv.sh
@@ -1,3 +1,4 @@
-export GOROOT=/usr/local/go 
-export GOPATH=/go # goの作業ディレクトリ任意、作成する必要がある
+# https://www.task-notes.com/entry/20170405/1491361200
+export GOROOT=/usr/local/go  # goがビルドするときに使うと思われる、作成する
+export GOPATH=/ec2-user/go # goの作業ディレクトリ任意、作成する必要がある
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
